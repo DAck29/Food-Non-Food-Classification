bb8279adc7eab547641f675b4df5f21271225dbf
nlpeUB <168057332+nlpeUB@users.noreply.github.com>
2024-12-13 20:22:06 +0100

    Update resnet_model.py
    Added docstring

--- resnet_model.py
+++ resnet_model.py
@@ -1,12 +1,20 @@
+# Import Libraries
 import torch
 import torch.nn as nn
 import torchvision.models as models
 from torchvision.models import ResNet50_Weights
 
-
-
 def get_resnet50_model(num_classes: int = 2, pretrained: bool = True) -> nn.Module:
+    """
+    This function returns a modified ResNet50 model for the number of classes specified.
+    
+    Params:
+    - num_classes (int): Number of output classes where default = 2.
+    - pretrained (bool): Set to true (default) -> uses pretrained weights from ResNet50
 
+    Returns:
+    - nn.Module: Modified ResNet50 model with an updated fully connected layer.
+    """
     # Load the pretrained ResNet50 model
     #model = models.resnet50(weights=ResNet50_Weights.DEFAULT)
     model = models.resnet50(weights=ResNet50_Weights.DEFAULT)
